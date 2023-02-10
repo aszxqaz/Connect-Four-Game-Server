ea34898029aca03aeb48a2e5a7fc53f64fd15b25
Maxim B <b.maxim.dev@gmail.com>
2023-02-10 21:44:39 +0400

    redis module added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  console.log(process.env)
 
   // app.use(session)
 
