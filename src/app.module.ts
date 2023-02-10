f106526de69ae3c36f5fe7a0e43f1c1c8044b861
Maxim B <b.maxim.dev@gmail.com>
2023-02-10 22:41:04 +0400

    authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { redisModule } from './redis/redis.module';
@@ -15,7 +13,7 @@ import { SessionModule } from './session/session.module';
     redisModule,
     SessionModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { SessionSerializer } from './session.serializer';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [],
+  providers: [SessionSerializer, LocalStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
